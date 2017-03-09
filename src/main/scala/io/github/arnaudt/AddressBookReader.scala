8bb18642f0fc5f6dadc3523cc27c943b8c5df4e3
Arnaud Tanguy <arnaudtanguy@gmail.com>
2017-03-09 19:30:33 +0000

    add the AddressBook file add a test to split a line to an array of string values

---
+++ src/main/scala/io/github/arnaudt/AddressBookReader.scala
@@ -0,0 +1,8 @@
+package io.github.arnaudt
+
+/**
+  * Created by arnaudtanguy on 09/03/2017.
+  */
+object AddressBookReader {
+  def splitContactLine(s: String): Array[String] = ???
+}
---
+++ src/test/scala/io/github/arnaudt/AddressBookReaderSpec.scala
@@ -0,0 +1,15 @@
+package io.github.arnaudt
+
+import org.specs2.mutable.Specification
+
+/**
+  * Created by arnaudtanguy on 09/03/2017.
+  */
+class AddressBookReaderSpec extends Specification {
+
+  "AddressBookReader" should {
+    "split a contact line to an array of contact values" in {
+      AddressBookReader.splitContactLine("Bill McKnight, Male, 16/03/77") === Array("Bill McKnight", "Male", "16/03/77")
+    }
+  }
+}
