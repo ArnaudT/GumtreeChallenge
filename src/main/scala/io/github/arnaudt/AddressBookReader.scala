28020555c4945295b2be8b7866dd51818ea6c14d
Arnaud Tanguy <arnaudtanguy@gmail.com>
2017-03-09 19:35:07 +0000

    implementation of the date parser

--- src/main/scala/io/github/arnaudt/AddressBookReader.scala
+++ src/main/scala/io/github/arnaudt/AddressBookReader.scala
@@ -1,14 +1,21 @@
 package io.github.arnaudt
 
-import java.time.LocalDate
+import java.text.SimpleDateFormat
+import java.time.{LocalDate, ZoneId}
 
 /**
   * Created by arnaudtanguy on 09/03/2017.
   */
 object AddressBookReader {
-  def parseDate(date: String): LocalDate = ???
+  val format = new SimpleDateFormat("dd/MM/yy")
 
   def splitContactLine(line: String): Array[String] = {
     line.split(',').map(_.trim)
   }
+
+  def parseDate(date: String): LocalDate = {
+    format.parse(date)
+      .toInstant().atZone(ZoneId.systemDefault()).toLocalDate()
+  }
+
 }
