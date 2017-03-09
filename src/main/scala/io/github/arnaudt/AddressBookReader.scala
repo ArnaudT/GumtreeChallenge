1ad8f0708d297c8ef7b7a36d2c02eea73006a7e1
Arnaud Tanguy <arnaudtanguy@gmail.com>
2017-03-09 19:51:32 +0000

    add specification to create a contact from the list of strings

--- src/main/scala/io/github/arnaudt/AddressBookReader.scala
+++ src/main/scala/io/github/arnaudt/AddressBookReader.scala
@@ -3,19 +3,26 @@ package io.github.arnaudt
 import java.text.SimpleDateFormat
 import java.time.{LocalDate, ZoneId}
 
+import scala.util.Try
+
 /**
   * Created by arnaudtanguy on 09/03/2017.
   */
 object AddressBookReader {
+
   val format = new SimpleDateFormat("dd/MM/yy")
 
   def splitContactLine(line: String): Array[String] = {
     line.split(',').map(_.trim)
   }
 
-  def parseDate(date: String): LocalDate = {
-    format.parse(date)
-      .toInstant().atZone(ZoneId.systemDefault()).toLocalDate()
+  def parseDate(date: String): Option[LocalDate] = {
+    Try(format.parse(date)
+      .toInstant().atZone(ZoneId.systemDefault()).toLocalDate()).toOption
   }
+
+  def createContact(fullName: String, sex: String, birthday: String): Try[Contact] = ???
+
+  def createContact(values: Array[String]): Option[Contact] = ???
 
 }
--- src/test/scala/io/github/arnaudt/AddressBookReaderSpec.scala
+++ src/test/scala/io/github/arnaudt/AddressBookReaderSpec.scala
@@ -4,18 +4,45 @@ import java.time.LocalDate
 
 import org.specs2.mutable.Specification
 
+import scala.util.Try
+
 /**
   * Created by arnaudtanguy on 09/03/2017.
   */
 class AddressBookReaderSpec extends Specification {
 
-  "AddressBookReader" should {
+  val billContact = Contact(
+    fullName = Contact.FullName("Bill McKnight"),
+    sex = Contact.Sex.Male,
+    birthday = Contact.Birthday(LocalDate.of(1977, 3, 16)))
+
+  "splitContactLine" should {
     "split a contact line to an array of contact values with any leading and trailing whitespace removed" in {
-      AddressBookReader.splitContactLine("Bill McKnight, Male, 16/03/77") === Array("Bill McKnight", "Male", "16/03/77")
+      AddressBookReader.splitContactLine("Bill McKnight, Male, 16/03/77") ==== Array("Bill McKnight", "Male", "16/03/77")
+    }
+  }
+
+  "parseDate" should {
+    "parse a date in string to a local date" in {
+      AddressBookReader.parseDate("16/03/77") ==== Some(LocalDate.of(1977, 3, 16))
     }
 
-    "parse a date in string to a local date" in {
-      AddressBookReader.parseDate("16/03/77") === LocalDate.of(1977, 3, 16)
+    "return None if it cannot parse a date" in {
+      AddressBookReader.parseDate("16-03/77") ==== None
+    }
+  }
+
+  "createContact" should {
+    "create a contact from string values" in {
+      AddressBookReader.createContact("Bill McKnight", "Male", "16/03/77") ==== Try(billContact)
+    }
+
+    "fail if the birthday is not valid" in {
+      AddressBookReader.createContact("Bill McKnight", "Male", "16-03-77").isFailure ==== true
+    }
+
+    "create a contact from an Array of string values" in {
+      AddressBookReader.createContact(Array("Bill McKnight", "Male", "16/03/77")) ==== Some(billContact)
     }
   }
 }
