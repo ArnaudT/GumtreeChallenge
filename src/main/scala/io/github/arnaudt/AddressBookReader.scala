00c9e82fb7a94b6f20e11cbf1f54056c3b2e862e
Arnaud Tanguy <arnaudtanguy@gmail.com>
2017-03-09 19:33:58 +0000

    add test to parse the birthday date

--- src/main/scala/io/github/arnaudt/AddressBookReader.scala
+++ src/main/scala/io/github/arnaudt/AddressBookReader.scala
@@ -1,9 +1,12 @@
 package io.github.arnaudt
 
+import java.time.LocalDate
+
 /**
   * Created by arnaudtanguy on 09/03/2017.
   */
 object AddressBookReader {
+  def parseDate(date: String): LocalDate = ???
 
   def splitContactLine(line: String): Array[String] = {
     line.split(',').map(_.trim)
--- src/test/scala/io/github/arnaudt/AddressBookReaderSpec.scala
+++ src/test/scala/io/github/arnaudt/AddressBookReaderSpec.scala
@@ -1,5 +1,7 @@
 package io.github.arnaudt
 
+import java.time.LocalDate
+
 import org.specs2.mutable.Specification
 
 /**
@@ -10,6 +12,10 @@ class AddressBookReaderSpec extends Specification {
   "AddressBookReader" should {
     "split a contact line to an array of contact values with any leading and trailing whitespace removed" in {
       AddressBookReader.splitContactLine("Bill McKnight, Male, 16/03/77") === Array("Bill McKnight", "Male", "16/03/77")
+    }
+
+    "parse a date in string to a local date" in {
+      AddressBookReader.parseDate("16/03/77") === LocalDate.of(1977, 3, 16)
     }
   }
 }
