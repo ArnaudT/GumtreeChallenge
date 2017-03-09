18520c877dcc773ef5b3bf48582756918a0aecfa
Arnaud Tanguy <arnaudtanguy@gmail.com>
2017-03-09 20:12:54 +0000

    parse a source to a list of contacts

--- src/main/scala/io/github/arnaudt/AddressBookReader.scala
+++ src/main/scala/io/github/arnaudt/AddressBookReader.scala
@@ -3,6 +3,9 @@ package io.github.arnaudt
 import java.text.SimpleDateFormat
 import java.time.{LocalDate, ZoneId}
 
+import io.github.arnaudt.Contact.Birthday
+
+import scala.io.Source
 import scala.util.Try
 
 /**
@@ -10,19 +13,31 @@ import scala.util.Try
   */
 object AddressBookReader {
 
-  val format = new SimpleDateFormat("dd/MM/yy")
-
   def splitContactLine(line: String): Array[String] = {
     line.split(',').map(_.trim)
   }
 
   def parseDate(date: String): Option[LocalDate] = {
+    val format = new SimpleDateFormat("dd/MM/yy")
     Try(format.parse(date)
       .toInstant().atZone(ZoneId.systemDefault()).toLocalDate()).toOption
   }
 
-  def createContact(fullName: String, sex: String, birthday: String): Try[Contact] = ???
+  def createContact(fullName: String, sex: String, birthday: String): Option[Contact] = {
+    for {
+      birthdayC <- parseDate(birthday).map(Birthday)
+      sexC <- Try(Contact.Sex.withName(sex)).toOption
+    } yield Contact(Contact.FullName(fullName), sexC, birthdayC)
+  }
 
-  def createContact(values: Array[String]): Option[Contact] = ???
+  def createContactFromArray(values: Array[String]): Option[Contact] = {
+    Try(createContact(values(0), values(1), values(2))).toOption.flatten
+  }
 
+  def parse(source: Source): List[Contact] = {
+    source.getLines()
+      .map(splitContactLine)
+      .flatMap(createContactFromArray)
+      .toList
+  }
 }
--- src/test/scala/io/github/arnaudt/AddressBookReaderSpec.scala
+++ src/test/scala/io/github/arnaudt/AddressBookReaderSpec.scala
@@ -4,7 +4,7 @@ import java.time.LocalDate
 
 import org.specs2.mutable.Specification
 
-import scala.util.Try
+import scala.io.Source
 
 /**
   * Created by arnaudtanguy on 09/03/2017.
@@ -14,7 +14,8 @@ class AddressBookReaderSpec extends Specification {
   val billContact = Contact(
     fullName = Contact.FullName("Bill McKnight"),
     sex = Contact.Sex.Male,
-    birthday = Contact.Birthday(LocalDate.of(1977, 3, 16)))
+    birthday = Contact.Birthday(LocalDate.of(1977, 3, 16))
+  )
 
   "splitContactLine" should {
     "split a contact line to an array of contact values with any leading and trailing whitespace removed" in {
@@ -24,7 +25,7 @@ class AddressBookReaderSpec extends Specification {
 
   "parseDate" should {
     "parse a date in string to a local date" in {
-      AddressBookReader.parseDate("16/03/77") ==== Some(LocalDate.of(1977, 3, 16))
+      AddressBookReader.parseDate("16/03/77") ==== Some(billContact.birthday.value)
     }
 
     "return None if it cannot parse a date" in {
@@ -34,15 +35,22 @@ class AddressBookReaderSpec extends Specification {
 
   "createContact" should {
     "create a contact from string values" in {
-      AddressBookReader.createContact("Bill McKnight", "Male", "16/03/77") ==== Try(billContact)
+      AddressBookReader.createContact("Bill McKnight", "Male", "16/03/77") ==== Some(billContact)
     }
 
     "fail if the birthday is not valid" in {
-      AddressBookReader.createContact("Bill McKnight", "Male", "16-03-77").isFailure ==== true
+      AddressBookReader.createContact("Bill McKnight", "Male", "16-03-77").isEmpty ==== true
     }
 
     "create a contact from an Array of string values" in {
-      AddressBookReader.createContact(Array("Bill McKnight", "Male", "16/03/77")) ==== Some(billContact)
+      AddressBookReader.createContactFromArray(Array("Bill McKnight", "Male", "16/03/77")) ==== Some(billContact)
+    }
+  }
+
+  "parse" should {
+    "generate contacts from the source" in {
+      val source = Source.fromString("Bill McKnight, Male, 16/03/77")
+      AddressBookReader.parse(source) ==== List(billContact)
     }
   }
 }
